9f36133f4f08e507161f2eb9effc07a9dc70d8c5
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2020-12-19 03:44:27 -0800

    Add sharding manager

---
+++ app.ts
@@ -0,0 +1,23 @@
+/*
+    The following code goes into it's own file, and you run this file
+    instead of your main bot file.
+*/
+
+// Include discord.js ShardingManger
+const { ShardingManager } = require('discord.js');
+const { config } = require('./config.json');
+import {} from "./adora"; 
+
+// Create your ShardingManger instance
+const manager = new ShardingManager('./adora.js', {
+    // for ShardingManager options see:
+    // https://discord.js.org/#/docs/main/v12/class/ShardingManager
+    totalShards: 'auto',
+    token: config.token
+});
+
+// Spawn your shards
+manager.spawn();
+
+// Emitted when a shard is created
+manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
