afcfe4ad2c716f92b6e7c6ed57a4e7c89c5abfa2
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-06-24 18:09:13 -0700

    move help page

--- src/modules/commandhandler.ts
+++ src/modules/commandhandler.ts
@@ -10,6 +10,7 @@ import { processAllModerationCommands, howManyUsersInBanDatabase } from "./moder
 import { updateDiscordBotsGG, updateDatadogCount } from "./uploadStatsToBotsGg"
 import {youtubeVideoStats} from "./youtube"
 import {sendVoteLinks,showTopVoters} from "./vote"
+import {helpDirectory} from "./help"
 const wiktionary = require('wiktionary')
 const isUrl = require("is-url");
 const scrapeyoutube = require('scrape-youtube').default;
@@ -123,92 +124,7 @@ export async function commandHandler(msg, client, config, cassandraclient, dogst
       }
 
       if (command === "help") {
-        //msg.channel.send("**Adora Commands**").catch(console.error());;
-        msg.channel.send({
-          "content": "**Adora Commands**",
-          "embeds": [{
-            "title": "Help - Music Charts & Statistics",
-            "description": "Access live information across music charts and platforms",
-            "fields": [
-              {
-                "name": "`a!billboard`",
-                "value": "View latest & historical Billboard Charts, run `a!billboard` for more info"
-              },
-              {
-                "name": "`a!bbp`",
-                "value": "View statistics for billboard polls, run command for more info"
-              },
-              {
-                "name": "`a!bv`",
-                "value": "Retrieve voting links for billboard polls, run command for a list of polls"
-              },
-              {
-                "name": "`a!youtube`",
-                "value": "`a!youtube <video link / search for a video>`: Realtime view counter for YouTube videos. \n Example: `a! youtube fake love music video` or `a! youtube https://www.youtube.com/watch?v=gdZLi9oWNZg`, run `a!youtube` for more information\nAlias: `a!yt`"
-              },
-              {
-                "name": "`a!lyrics [Search Term]`",
-                "value": "Shows lyrics of a song from Genius"
-              }
-            ]
-          },
-          {
-            "title": "Help - Moderation",
-            "description": "Make protecting your community easier!",
-            "fields": [
-              {
-                "name": "`a!autoban`",
-                "value": "Automatically block known-raid accounts from blacklists before they come to your server, run command for more info"
-              },
-              {
-                "name": "`a!ban (mentions/userids) [reason]`",
-                "value": "a!ban can ban as many users via mention or user ids with an optional reason"
-              },
-              {
-                "name": "`a!banpurge (mentions/userids) [reason]`",
-                "value": "Same thing as `a!ban` but also deletes all the users messages. Good for raids."
-              },
-              {
-                "name": "`a!unban (mentions/userids) [reason]`",
-                "value": "Unban as many users via mention or user ids with an optional reason"
-              },
-              {
-                "name": "`a!wrongfulban`",
-                "value": "Report wrongful bans applied by the Adora system and we'll investigate and unban as quickly as we can."
-              },
-              {
-                "name": "`a!inspect  (mentions/userids)`",
-                "value": "Shows info of user like Flags, Account creation time, Banlist status, and icon!"
-              }
-            ]
-          },
-          {
-            "title": "Help - Adora",
-            "description": "General tools and access!",
-            "fields": [
-              {
-                "name": "`a!ping`",
-                "value": "Pong! Returns the bot's latency to Discord's servers."
-              },
-              {
-                "name": "`a!invite`",
-                "value": "Invite the bot to all your friend's servers! Shows invite link and support server."
-              },
-              {
-                "name": "`a!botstats`",
-                "value": "Shows adora bot statistics"
-              },
-              {
-                "name": "I have an idea for a command or feedback!",
-                "value": "We'd love to hear it! Please join our support server and tell us! Run `a!invite` for the invite link to our Support & Suggestion Adorabot Discord Server"
-              },
-              {
-                "name": "Credits",
-                "value": "Developer: Kyler#9100\nAdmins: Moka\nThank you to all that contribute code, feedback, and ban reports! Y'all make this bot better so thanks for using it"
-              }
-            ]
-          }]
-        }).catch(console.error());
+        helpDirectory({message: msg,command,args})
       }
 
       if (command === "translate") {
---
+++ src/modules/help.ts
@@ -0,0 +1,105 @@
+import {Message} from 'discord.js';
+
+interface helpDirArgs {
+    message: Message,
+    command: any,
+    args: any,
+    [key:string]: any
+}
+
+export function helpDirectory(helpDirArgs: helpDirArgs) {
+    //msg.channel.send("**Adora Commands**").catch(console.error());;
+    helpDirArgs.message.channel.send({
+        "content": "**Adora Commands**",
+        "embeds": [{
+          "title": "Help - Music Charts & Statistics",
+          "description": "Access live information across music charts and platforms",
+          "fields": [
+            {
+              "name": "`a!billboard`",
+              "value": "View latest & historical Billboard Charts, run `a!billboard` for more info"
+            },
+            {
+              "name": "`a!bbp`",
+              "value": "View statistics for billboard polls, run command for more info"
+            },
+            {
+              "name": "`a!bv`",
+              "value": "Retrieve voting links for billboard polls, run command for a list of polls"
+            },
+            {
+              "name": "`a!youtube`",
+              "value": "`a!youtube <video link / search for a video>`: Realtime view counter for YouTube videos. \n Example: `a! youtube fake love music video` or `a! youtube https://www.youtube.com/watch?v=gdZLi9oWNZg`, run `a!youtube` for more information\nAlias: `a!yt`"
+            },
+            {
+              "name": "`a!lyrics [Search Term]`",
+              "value": "Shows lyrics of a song from Genius"
+            }
+          ]
+        },
+        {
+          "title": "Help - Moderation",
+          "description": "Make protecting your community easier!",
+          "fields": [
+            {
+              "name": "`a!autoban`",
+              "value": "Automatically block known-raid accounts from blacklists before they come to your server, run command for more info"
+            },
+            {
+              "name": "`a!ban (mentions/userids) [reason]`",
+              "value": "a!ban can ban as many users via mention or user ids with an optional reason"
+            },
+            {
+              "name": "`a!banpurge (mentions/userids) [reason]`",
+              "value": "Same thing as `a!ban` but also deletes all the users messages. Good for raids."
+            },
+            {
+              "name": "`a!unban (mentions/userids) [reason]`",
+              "value": "Unban as many users via mention or user ids with an optional reason"
+            },
+            {
+              "name": "`a!wrongfulban`",
+              "value": "Report wrongful bans applied by the Adora system and we'll investigate and unban as quickly as we can."
+            },
+            {
+              "name": "`a!inspect  (mentions/userids)`",
+              "value": "Shows info of user like Flags, Account creation time, Banlist status, and icon!"
+            }
+          ]
+        },
+        {
+          "title": "Help - Adora",
+          "description": "General tools and access!",
+          "fields": [
+            {
+              "name": "`a!ping`",
+              "value": "Pong! Returns the bot's latency to Discord's servers."
+            },
+            {
+              "name": "`a!invite`",
+              "value": "Invite the bot to all your friend's servers! Shows invite link and support server."
+            },
+            {
+              "name": "`a!botstats`",
+              "value": "Shows adora bot statistics"
+            },
+            {
+                "name": "`a!vote`",
+                "value": "Vote for Adora on Top.gg links"
+            },
+            {
+                "name": "`a!votes`",
+                "value": "Shows leaderboard of voters"
+            }
+            {
+              "name": "I have an idea for a command or feedback!",
+              "value": "We'd love to hear it! Please join our support server and tell us! Run `a!invite` for the invite link to our Support & Suggestion Adorabot Discord Server"
+            },
+            {
+              "name": "Credits",
+              "value": "Developer: Kyler#9100\nAdmins: Moka\nThank you to all that contribute code, feedback, and ban reports! Y'all make this bot better so thanks for using it"
+            }
+          ]
+        }]
+      }).catch(console.error());
+}
