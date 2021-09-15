376f5cd997dd481fc3f6adedb84453341654e201
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-09-14 22:30:03 -0700

    discord together

---
+++ src/modules/discordTogether.ts
@@ -0,0 +1,44 @@
+interface ytpartyargs {
+    message: any;
+    client: any;
+}
+
+export function ytparty(ytpartyargs: ytpartyargs) {
+    if (ytpartyargs.message.guild) {
+      //  console.log(ytpartyargs.message.member.voice)
+     //   if(ytpartyargs.message.member.voice.channel) {
+         if (ytpartyargs.message.member.voice.channel) {
+            ytpartyargs.client.discordTogether.createTogetherCode(ytpartyargs.message.member.voice.channel.id, 'youtube').then(async invite => {
+                return ytpartyargs.message.reply({embeds: [{
+                    "title": `Click here to start the YouTube Party in ${ytpartyargs.message.member.voice.channel.name}`,
+                    "url": `${invite.code}`
+                }]});
+            });
+         }  
+        else {
+          return ytpartyargs.message.reply('Join a voice channel to use YouTube Party!')
+        }
+    } else {
+        return ytpartyargs.message.reply('YouTube Parties can only be started in a server.')
+    }
+}
+
+export function fishing(ytpartyargs: ytpartyargs) {
+    if (ytpartyargs.message.guild) {
+      //  console.log(ytpartyargs.message.member.voice)
+     //   if(ytpartyargs.message.member.voice.channel) {
+         if (ytpartyargs.message.member.voice.channel) {
+            ytpartyargs.client.discordTogether.createTogetherCode(ytpartyargs.message.member.voice.channel.id, 'fishing').then(async invite => {
+                return ytpartyargs.message.reply({embeds: [{
+                    "title": `Click here to start the Fishing Game in ${ytpartyargs.message.member.voice.channel.name}`,
+                    "url": `${invite.code}`
+                }]});
+            });
+         }  
+        else {
+          return ytpartyargs.message.reply('Join a voice channel to play the Fishing game!')
+        }
+    } else {
+        return ytpartyargs.message.reply('The Fishing Game can only be started in a server.')
+    }
+}
