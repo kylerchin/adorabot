ff4ba432a9ff317459ea8bc2bb9f228cd6e1b1da
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-09-21 13:04:53 -0700

    delete old file

--- billboardtest.js
+++
@@ -1,8 +0,0 @@
-const { listCharts,getChart } = require('billboard-top-100');
-
-listCharts((err, charts) => {
-    if (err) console.log(err);
-    console.log(charts); // prints array of all charts
-
-    
-  });
