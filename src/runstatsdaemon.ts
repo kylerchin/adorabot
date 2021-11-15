3cbc294e886b53ae940477db15427f7c82760ed3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-11-15 10:15:28 -0800

    garbage collection

--- src/runstatsdaemon.ts
+++ src/runstatsdaemon.ts
@@ -7,5 +7,10 @@ fetchStatsForAll()
 
 setInterval(function() {
     fetchStatsForAll()
+    try {
+        if (global.gc) {global.gc();}
+      } catch (e) {
+        
+      }
 }, 60 * 1000 * 10);// your code goes here... // 60 * 1000 milsec
 //addVideoToTrackList("_EEo-iE5u_A")
