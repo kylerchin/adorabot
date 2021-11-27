9faa0cf4fa4f30240f1fcd278adee2c62ea41657
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-11-26 18:50:51 -0800

    more console log

--- src/modules/get2021mamavoteinfo.ts
+++ src/modules/get2021mamavoteinfo.ts
@@ -2,11 +2,34 @@ import axios from 'axios'
 
 export function getMama2021Score() {
    return new Promise(async (resolve, reject) => {
-        axios.get('https://mama.mwave.me/en/preVoteResult')
+    var config:any = {
+        method: 'get',
+        url: 'https://mama.mwave.me/en/preVoteResult',
+        headers: { 
+          'Cookie': 'org.springframework.mobile.device.site.CookieSitePreferenceRepository.SITE_PREFERENCE=NORMAL; JSESSIONID=5445A4CAD399008D9AE8BEEA8CAA8270; SCOUTER=x3qrab1aj39ikj'
+        }
+      };
+      
+      axios(config)
   .then(function (response) {
     // handle success
     console.log(response);
-    resolve(response)
+
+    console.log(response.data)
+
+    var webpage = response.data;
+
+    var arrayOfMatchingCan = webpage.match(/var candidates = \[([^\]])*\]/g)
+
+    if (arrayOfMatchingCan) {
+      var candidatesJson = JSON.parse(arrayOfMatchingCan[0].replace(/var candidates = /g,""))
+
+      console.log(candidatesJson)
+    }
+ else {
+   reject("nothing found")
+ }
+ 
   })
   .catch(function (error) {
     // handle error
---
+++ src/modules/testmama.ts
@@ -0,0 +1,3 @@
+import {getMama2021Score} from './get2021mamavoteinfo'
+
+getMama2021Score()
