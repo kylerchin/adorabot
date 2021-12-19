eae635632dc6f98f5795deac3fa9223859be8c90
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-12-18 21:03:17 -0800

    api tester

---
+++ src/testyoutubekeys.ts
@@ -0,0 +1,36 @@
+const editJsonFile = require("edit-json-file");
+
+var authconfigfile = editJsonFile(`${__dirname}/../config.json`);
+var loadedAuthData = authconfigfile.get()
+
+const requestjson = require('request-json');
+
+var videoname = 'WMweEpGlu_U'
+
+loadedAuthData.config.youtubeApiKeysDaemon.forEach(async (eachApiKey) => {
+    console.log(eachApiKey)
+    
+    var success = false
+
+    const pathForYtRequest = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics,status,liveStreamingDetails&id=" + videoname + "&key=" + eachApiKey;
+
+    var youtubeclient = requestjson.createClient('https://youtube.googleapis.com/');
+
+    
+
+  await  youtubeclient.get(pathForYtRequest, async function (err, res, body) {
+        if (err) {
+            success = false
+        }
+
+        if (body.items) {
+            if (body.items[0].snippet.title) {
+                success = true
+            }
+        }
+
+
+
+        console.log(`${success} | ${eachApiKey}`)
+    })
+})
