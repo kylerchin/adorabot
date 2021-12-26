f27234f694c6079ff1b295ce5b3af16af7230fc4
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-12-26 15:19:52 -0800

    reduce to 2000 ms per ban

--- src/modules/moderation.ts
+++ src/modules/moderation.ts
@@ -1007,7 +1007,9 @@ export async function everyServerRecheckBans(cassandraclient, client, recheckUnk
                                         //    console.log('step 4: server available')
                                             if (true) {
                                           //      console.log('step 5, pass algo')
-                                                var timeoutAmount = 3000 * (howManyBansHaveBeenSubmittedSoFar + 1)
+
+
+                                                var timeoutAmount = 2000 * (howManyBansHaveBeenSubmittedSoFar + 1)
 
                                                 //  console.log(`the current timeout amount is: ${timeoutAmount}`)
                                    
