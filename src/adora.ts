d36a68447bb65903551886e2c717b490b93e2e9c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-04-02 09:58:42 -0700

    lol what the frick

--- src/adora.ts
+++ src/adora.ts
@@ -113,16 +113,38 @@ try {
   // in the future... restrict to 1 shard
   try {
     if ( recievedFirstMessageState) {
+
+      await logger.discordInfoLogger.info("recievedfirstmsg", {type: 'votereminddebug'});
+
       cassandraclient.execute("SELECT * FROM adoravotes.pendingvotereminders", {prepare: true})
       .then((adoravotescassandra) => {
-        adoravotescassandra.forEach((eachRow) => {
+      
+        var latestTopggVoteTimes = {
+
+        }
+
+        var latestDblVoteTimes = {
+
+        }
+
+        adoravotescassandra.rows.forEach((eachRow) => {
+          if (eachRow.service == "topgg") {
+            latestTopggVoteTimes[eachRow.userid] = eachRow.time.getDate().getTime();
+          } else {
+            latestDblVoteTimes[eachRow.userid] = eachRow.time.getDate().getTime();
+          }
+        })
+
+        adoravotescassandra.rows.forEach((eachRow) => {
           
           // check if more than 12 hours ago
   
           if (eachRow.time.getDate().getTime() < Date.now() - twelvehours) {
            var valueofremindedalready = adoravotesremindedalready.get(`${eachRow.time}-${eachRow.service}`);
   
-           if (valueofremindedalready === undefined) {
+            var unixtimeofvote = eachRow.time.getDate().getTime()
+
+           if (valueofremindedalready == undefined) {
   
             //save in cache
             adoravotesremindedalready.set(`${eachRow.time}-${eachRow.service}`,true);
@@ -131,29 +153,36 @@ try {
               eachRow.time
             ], {prepare: true})
             .then((deleterowsuccess) => {
-              client.users.fetch(eachRow.userid).then((user) => {
-                try {
-                  var stringToSend;
-
-                  if (eachRow.service === 'topgg') {
-                    stringToSend = "Thank you so much for voting earlier for Adora! You're eligable to vote again on Top.gg! \nEvery vote is 1 ticket in the monthly nitro giveaway! \n  Here's the link if you need it :purple_heart:  https://top.gg/bot/737046643974733845/vote"
-                  }
-                  if (eachRow.service === "discordbotlist") {
-                    stringToSend = "Thank you so much for voting earlier for Adora! You're eligable to vote again on Discord Bot List! \nEvery vote is 1 ticket in the monthly nitro giveaway! \n Here's the link if you need it :purple_heart: https://discordbotlist.com/bots/adora-ahelp/upvote"
-                  }
-                  user.send();	
-                } catch (err){
-                  console.error(err);
-                }
-            })
-            .catch(error => {
-              console.error(error)
-            })
+              if (latestTopggVoteTimes[eachRow.userid] == unixtimeofvote || latestDblVoteTimes[eachRow.userid] == unixtimeofvote) {
+                client.users.fetch(eachRow.userid).then((user) => {
+                  try {
+                    var stringToSend;
   
-           }
+                    if (eachRow.service === 'topgg') {
+                      stringToSend = "Thank you so much for voting earlier for Adora! You're eligable to vote again on Top.gg! \nEvery vote is 1 ticket in the monthly nitro giveaway! \n  Here's the link if you need it :purple_heart:  https://top.gg/bot/737046643974733845/vote"
+                    }
+                    if (eachRow.service === "discordbotlist") {
+                      stringToSend = "Thank you so much for voting earlier for Adora! You're eligable to vote again on Discord Bot List! \nEvery vote is 1 ticket in the monthly nitro giveaway! \n Here's the link if you need it :purple_heart: https://discordbotlist.com/bots/adora-ahelp/upvote"
+                    }
+                    user.send();	
+                  } catch (err){
+                    console.error(err);
+                  }
+              })
+              .catch(error => {
+                console.error(error)
+              })
+              }
+          
+  
+           });
           }
+
   
-        })
+        }
+        
+      });
+
       })
       .catch(error => {
         console.error(error)
