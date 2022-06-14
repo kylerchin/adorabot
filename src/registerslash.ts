44aea34ed0d6893c1d32de7679d4643aee77a38c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-06-14 14:44:00 -0700

    new slashes

--- src/registerslash.ts
+++ src/registerslash.ts
@@ -42,7 +42,7 @@ const commands = [
 {
   "name": "mama",
   "description": "View Real-Time Charts for MAMA Awards 2021: Worldwide Fans’ Choice TOP 10"
-},*/
+},
 {
   "name": "billboard",
   "description": "View latest & historical Billboard Charts",
@@ -58,6 +58,80 @@ const commands = [
       "name": "date",
       "description": "Optional Date for Historical charts. \"YYYY-MM-DD\" format, dashes required"
     }
+  ]
+},*/
+{
+  "name": "ban",
+  "description": "Bans users, only works in server for users with ban perms",
+  "options": [
+    {
+      "type": 3,
+      "name": "bans",
+      "description": "Insert 1 or more userids and/or mentions to ban",
+      "required": true
+    },
+    {
+      "type": 5,
+      "name": "purgemsgs",
+      "description": "If enabled, deletes messages in this server from the users"
+    }
+  ]
+},
+{
+  "name": "unban",
+  "description": "Unbans users, only works in server for users with ban perms",
+  "options": [
+    {
+      "type": 3,
+      "name": "bans",
+      "description": "Insert 1 or more userids and/or mentions to unban",
+      "required": true
+    }
+  ]
+},
+{
+  "name": "Kick",
+  "description": "Kick users, only works in server for users with kick perms",
+  "options": [
+    {
+      "type": 3,
+      "name": "bans",
+      "description": "Insert 1 or more userids and/or mentions to kick",
+      "required": true
+    }
+  ]
+},
+{
+  "name": "autoban",
+  "description": "Automatically block known-raid accounts from blacklists before they come to your server,/autoban help for more info",
+  "options": [
+    {
+      "type": 1,
+      "name": "on",
+      "description": "Enables Autoban on this server"
+    },
+    {
+      "type": 1,
+      "name": "off",
+      "description": "Disables Autoban on this server"
+    },
+    {
+      "type": 1,
+      "name": "help",
+      "description": "About Adora's automated banlist & status for this server"
+    }
+  ]
+},
+{
+  "name": "inspect",
+  "description": "Shows info of user like Flags, Account creation time, Banlist status, and icon!",
+  "options": [
+    {
+      "type": 3,
+      "name": "bans",
+      "description": "Insert 1 or more userids and/or mentions to inspect",
+      "required": true
+    },
   ]
 }
 ];
