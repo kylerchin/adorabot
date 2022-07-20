5ef678339b55f122c5177679db48fc1a87e59bcf
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-07-19 21:45:03 -0700

    add periods to warning msg

--- src/modules/commandhandler.ts
+++ src/modules/commandhandler.ts
@@ -52,8 +52,8 @@ export async function commandHandler(msg, config, dogstatsd, startupTime) {
     
     //warn users about discord ending message services
     msg.reply("Discord will be disabling bots reading messages starting August 31, 2022.\n" + 
-    "Users of Adora should switch to Adora's Slash Commands\n"+
-    "If Slash Commands aren't showing up in your server, you'll need to give Adora permissions again\n" +
+    "Users of Adora should switch to Adora's Slash Commands.\n"+
+    "If Slash Commands aren't showing up in your server, you'll need to give Adora permissions again.\n" +
     "Click this link to give Adora slash permissions in your server. https://discord.com/oauth2/authorize?client_id=737046643974733845&scope=bot%20applications.commands&permissions=2151017550\n" + 
     "Run /help for a list of commands and /invite for our support server."
     );
