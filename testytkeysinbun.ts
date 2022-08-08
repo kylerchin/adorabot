8e8c4327cde6c7cece81789bab267af6a37e1d39
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-08-07 23:29:24 -0700

    script with func

---
+++ testytkeysinbun.ts
@@ -0,0 +1,60 @@
+
+import {config} from './config.json';
+import axios from 'axios';
+
+  
+
+       
+
+        function testfunction(category, list) {
+
+            var statuses = {}
+
+           list.forEach((key, keyIndex) => {
+
+                statuses["daemon" + keyIndex] = "Loading...";
+      
+              });
+
+           list.forEach(async (key, keyIndex) => {
+    
+                const pathForYtRequest = "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=hr-325mclek&key=" + key;
+      
+                await axios.get(pathForYtRequest)
+                .then(async (response:any) => {
+      
+                 const body = JSON.parse(response.data);
+      
+                 if (body.items) {
+                    if (body.items[0].statistics) {
+                      statuses[category + keyIndex] = "Success";
+                      
+                  console.log(body);
+                    } else {
+                      statuses[category+ keyIndex] = "No stats";
+                      
+                  console.log(body);
+                    }
+                 } else {
+                  
+                  statuses[category + keyIndex] = "No items found";
+                  console.log(body);
+      
+                 }
+                  
+                }).catch((error:any) => {
+                    
+                statuses[category + keyIndex] = "Failed! Catch axios";
+      
+                console.error(error);
+                });
+      
+                
+                console.log(statuses);
+      
+              });
+      
+        }
+      
+        testfunction("daemon",  config.youtubeApiKeysDaemon);
+        testfunction("user",  config.youtubeApiKeys);
