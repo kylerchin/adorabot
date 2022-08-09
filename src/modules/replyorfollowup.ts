d6f20019d66a483a8445f4bb1c85b513e8612ff1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-08-08 19:10:56 -0700

    check if message exists even

--- src/modules/replyorfollowup.ts
+++ src/modules/replyorfollowup.ts
@@ -7,11 +7,15 @@ export function replyorfollowup(options: replyOrFollowUpInterface) {
     var {messageorinteraction, content} = options;
 
     try {
+        if (messageorinteraction) {
+            
         if (typeof messageorinteraction.commandName != "undefined") {
             return messageorinteraction.followUp(content)
     } else {
        return messageorinteraction.reply(content)
     }
+        }
+
     } catch (error) {
         console.error(error);
         return false;
