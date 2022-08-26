2b251fcff14aa8116c96b853fe0358d99bb19c3e
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-08-26 09:57:22 -0700

    another catch failsafe

--- src/modules/replyorfollowup.ts
+++ src/modules/replyorfollowup.ts
@@ -14,9 +14,17 @@ export function replyorfollowup(options: replyOrFollowUpInterface) {
             
         if (typeof messageorinteraction.commandName != "undefined") {
             if (messageorinteraction.deferred) {
-                return messageorinteraction.followUp(content)
+                return messageorinteraction.followUp(content).catch((error) => {
+                    messageorinteraction.reply(content).catch((error) => {console.log(error)})
+                    console.error(error);
+                    uploadStringToNewRelic(JSON.stringify({...error, type: "replyorfollowuperror"}));
+                })
             } else {
-                return messageorinteraction.reply(content)
+                return messageorinteraction.reply(content).catch((error) => {
+                    messageorinteraction.followUp(content).catch((error) => {console.log(error)});
+                    console.error(error);
+                    uploadStringToNewRelic(JSON.stringify({...error, type: "replyorfollowuperror"}));
+                })
             }
     } else {
        return messageorinteraction.reply(content)
